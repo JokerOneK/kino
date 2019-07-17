c356506e10a625fc10ba5b5513d51a33aa6bb9f0
Altay Toktassyn <altosha211@gmail.com>
2019-07-17 12:04:57 +0600

    select test

--- sel.py
+++ sel.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 
 class PythonOrgSearch(unittest.TestCase):
@@ -11,13 +12,19 @@ class PythonOrgSearch(unittest.TestCase):
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get("http://www.kino.kz")
+
         # self.assertIn("Python", driver.title)
 
-        elem = driver.find_element_by_xpath("//select[@name='name']")
-        all_options = elem.find_element_by_tag_name("option")
-        for option in all_options:
-            print("Value is: %s" % option.get_attribute("value"))
-            option.click()
+        # elem = driver.find_element_by_xpath("//select[@name='city-select']")
+        # all_options = elem.find_elements_by_tag_name("option")
+        # for option in all_options:
+        #     print("Value is: %s" % option.get_attribute("value"))
+        #     option.click()
+
+        select = Select(driver.find_element_by_name('city-select'))
+        select.select_by_value("21")
+
+
 
         assert "No results found" not in driver.page_source
 
