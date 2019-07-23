2b4e37d2a1ca6dd4df999ff343995a57aacba8fa
Altay Toktassyn <altosha211@gmail.com>
2019-07-23 16:35:37 +0600

    working

--- sel.py
+++ sel.py
@@ -2,6 +2,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+import time
 
 
 class PythonOrgSearch(unittest.TestCase):
@@ -10,24 +11,18 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Safari()
 
     def test_search_in_python_org(self):
+
         driver = self.driver
         driver.get("http://www.kino.kz")
+        lis = []
 
-        # self.assertIn("Python", driver.title)
-
-        # elem = driver.find_element_by_xpath("//select[@name='city-select']")
-        # all_options = elem.find_elements_by_tag_name("option")
-        # for option in all_options:
-        #     print("Value is: %s" % option.get_attribute("value"))
-        #     option.click()
-
-        select = Select(driver.find_element_by_name('city-select'))
-        select.select_by_value("21")
-
-
+        elem = driver.find_element_by_xpath("//select[@name='city-select']")
+        all_options = elem.find_elements_by_tag_name("option")
+        for option in all_options:
+            lis.append(option.get_attribute("city-select"))
+        print(lis)
 
         assert "No results found" not in driver.page_source
-
     def tearDown(self):
         self.driver.close()
 
