21af711c91d16c3b5741532e1b8fbf1ef92ce047
Altay Toktassyn <altosha211@gmail.com>
2019-07-24 23:53:24 +0600

    added cinemas list

--- sel.py
+++ sel.py
@@ -1,11 +1,10 @@
-import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
 
 
-driver = webdriver.Safari()
+driver = webdriver.Chrome()
 
 driver.get("http://www.kino.kz")
 
@@ -20,9 +19,29 @@ icons = driver.find_elements_by_class_name("mov_title_block")       # расрк
 for icon in icons:
     driver.execute_script("arguments[0].click();", icon)
 
+
 schedules = driver.find_elements_by_class_name("arr_select")        # раскрыть всё расписание
 for schedule in schedules:
     driver.execute_script("arguments[0].click();", schedule)
 
+
+cells = []
+tds = driver.find_elements_by_class_name("txt-rounded")
+for td in tds:
+    cells.append(td.get_attribute("innerHTML"))
+
+# index = 0
+# table = driver.find_element_by_class_name("schedule")
+# rows = table.find_element_by_tag_name("tr")
+# rows.ge
+
+
+# print(cin)
+
+cinemas = driver.find_elements_by_xpath("//*[@id='seances_8165']/tbody/tr/td[1]/a")
+print([cinema.text for cinema in cinemas])
+
+print(cells)
 time.sleep(2)
 driver.close()
+
